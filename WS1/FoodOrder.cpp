e5c1109b698d76ff13dcb8d20ce1118d7e6cfba4
Juan Cuéllar <96099514+juan617dtu@users.noreply.github.com>
2025-12-09 16:39:39 -0500

    Add files via upload

---
+++ WS1/FoodOrder.cpp
@@ -0,0 +1,73 @@
+//
+// Created by masti on 12/8/2025.
+//
+
+#include "FoodOrder.h"
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+
+double g_dailydiscount = 0.0;
+double g_taxrate = 0.0;
+
+namespace seneca {
+
+    void FoodOrder::read(std::istream& is) {
+
+        if (is) {
+            is.getline(m_customer_name, 10, ',');
+            if (m_food_desc) delete [] m_food_desc;
+            std::string buffer;
+            getline(is, buffer, ',');
+            m_food_desc = new char[buffer.length() + 1];
+            strcpy(m_food_desc, buffer.c_str());
+            is >> m_food_price;
+            char temp = 'N';
+            is.ignore();
+            is >> temp;
+            if (temp == 'N') m_is_daily_special = false;
+            else m_is_daily_special = true;
+        }
+    }
+    void FoodOrder::display() const {
+
+        static size_t count = 0;
+        std::cout << std::left << std::setw(2) << ++count << ". ";
+        if (m_customer_name[0]) {
+            std::cout << std::setw(10) << m_customer_name << '|'
+            << std::setw(25) << m_food_desc << '|'
+            << std::fixed << std::setw(12) << std::setprecision(2)
+            << m_food_price + (m_food_price * g_taxrate) << '|';
+            if (m_is_daily_special) {
+                std::cout << std::right << std::setw(13) <<std::setprecision(2)
+                << (m_food_price + (m_food_price * g_taxrate)) - g_dailydiscount;
+            }
+        }
+        else { std::cout << "No Order"; }
+        std::cout << '\n';
+    }
+    FoodOrder::~FoodOrder() {
+
+        delete [] m_food_desc;
+        m_food_desc = nullptr;
+    }
+    FoodOrder::FoodOrder(const FoodOrder &other) {
+
+        *this = other;
+    }
+    FoodOrder &FoodOrder::operator=(const FoodOrder &other) {
+
+        if (this != &other) {
+            delete [] m_food_desc;
+            m_food_desc = nullptr;
+            strcpy(m_customer_name, other.m_customer_name);
+            m_food_price = other.m_food_price;
+            m_is_daily_special = other.m_is_daily_special;
+            if (other.m_food_desc) {
+                m_food_desc = new char[strlen(other.m_food_desc) + 1];
+                strcpy(m_food_desc, other.m_food_desc);
+            }
+        }
+        return *this;
+    }
+}
---
+++ WS1/FoodOrder.h
@@ -0,0 +1,30 @@
+//
+// Created by masti on 12/8/2025.
+//
+
+#ifndef SENECA_FOODORDER_H
+#define SENECA_FOODORDER_H
+#include <iostream>
+
+extern double g_taxrate;
+extern double g_dailydiscount;
+
+namespace seneca {
+
+    class FoodOrder {
+
+        char m_customer_name[10]{};
+        char* m_food_desc{};
+        double m_food_price{};
+        bool m_is_daily_special{};
+
+    public:
+        void read(std::istream& is);
+        void display() const;
+        FoodOrder(){};
+        FoodOrder(const FoodOrder& other);
+        FoodOrder& operator=(const FoodOrder& other);
+        ~FoodOrder();
+    };
+}
+#endif //FOODORDER_H
